a43a9dc11102fe6299010dc9139ea134af7f1beb
jonathanTIE <jonathan_bleu@hotmail.fr>
2020-12-01 19:19:56 +0100

    initial

--- Server/views.py
+++ Server/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, url_for, render_template, request, redirect, session
+from Server.Data import db
+from Server.controller import Auth
 
 app = Flask(__name__)
 app.static_folder = "../static"
@@ -12,11 +14,26 @@ def index():
     return render_template("accueil.html")
 
 @app.route('/vigie')
-def index():
+def vigie():
 
     return render_template("vigie.html")
 
-@app.route('/Webmaster')
-def index():
+@app.route('/webmaster')
+def webmaster():
 
     return render_template("webmaster.html")
+
+@app.route('/connecter', methods=['POST','GET'])
+def connecter():
+    print(request.form)
+    msg = Auth.connect_account(request)
+
+    if msg == "echec":
+        return render_template("index.html", infoC = "Echec d'authentification")
+    else:
+        return redirect(url_for('vigie'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
