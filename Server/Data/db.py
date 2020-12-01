60f151c1c8e7af5f204ea25e524c41b81346665a
jonathanTIE <jonathan_bleu@hotmail.fr>
2020-12-01 19:22:43 +0100

    initial

---
+++ Server/Data/db.py
@@ -0,0 +1,132 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+
+        'user': 'gsea',
+        'password': '*gsea*',
+        'host': 'localhost',
+        'port': 3306,  # 8889 pour les macs
+        'database': 'GSEA19B_Jonathan_Corentin',  # nom de la BDD
+        'raise_on_warnings': True,
+}
+
+#################################################################################################################
+# connexion au serveur de la base de données
+
+
+def connexion():
+    cnx = ""
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Mauvais login ou mot de passe")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("La Base de données n'existe pas.")
+        else:
+            print(err)
+    return cnx
+
+
+#################################################################################################################
+# fermeture de la connexion au serveur de la base de données
+
+
+def close_bd(cursor, cnx):
+    cursor.close()
+    cnx.close()
+
+
+###################################################################################################
+# transforme le résultat de la requete select en dictionnaire ayant pour index le nom des colonnes de la table en BD
+
+
+def convert_dictionnary(cursor):
+    columns = cursor.description
+    result = []
+    # reception des données sous forme de dictionnaire avec le nom des colonnes.
+    for value in cursor.fetchall():
+        tmp = {}
+        for (index, column) in enumerate(value):
+            tmp[columns[index][0]] = column
+        result.append(tmp)
+    return result
+"""
+def get_membreData():
+
+    try:
+        cnx = connexion()
+        cursor = cnx.cursor()
+        sql = "SELECT * FROM membre"
+        cursor.execute(sql)
+        res = convert_dictionnary(cursor)
+        close_bd(cursor,cnx)
+
+    except mysql.connector.Error as err:
+        res = "Failed get membre Data: {}".format(err)
+
+    return res
+
+def del_membreData(idUser):
+    try:
+        cnx = connexion()
+        cursor = cnx.cursor()
+        sql = "DELETE FROM membre WHERE idUser=%s"
+        param = (idUser,)
+        cursor.execute(sql, param)
+        cnx.commit()
+        msg = "suppOK"
+        close_bd(cursor,cnx)
+
+    except mysql.connector.Error as err:
+        msg = "Failed delete membre Data: {}".format(err)
+
+    return msg
+
+def add_membreData(nom, idEquipe):
+    try:
+        cnx = connexion()
+        cursor = cnx.cursor()
+        sql = "INSERT INTO membre (nom, idEquipe) VALUES (%s, %s)"
+        param = (nom, idEquipe)
+        cursor.execute(sql, param)
+        cnx.commit()
+        msg = "addOK"
+        close_bd(cursor,cnx)
+
+    except mysql.connector.Error as err:
+        msg = "Failed add membre Data: {}".format(err)
+
+    return msg
+
+def update_membreData(champ,idUser,newvalue):
+    try:
+        cnx = connexion()
+        cursor = cnx.cursor()
+        sql = "UPDATE membre SET "+champ+" = %s WHERE idUser = %s;"
+        param = (newvalue, idUser)
+        cursor.execute(sql, param)
+        cnx.commit()
+        msg = "updateOK"
+        close_bd(cursor,cnx)
+
+    except mysql.connector.Error as err:
+        msg = "Failed update membre data: {}".format(err)
+
+    return msg
+"""
+def getAuthData(login,mdp):
+    try:
+        cnx = connexion()
+        cursor = cnx.cursor()
+        sql = "SELECT * FROM identification WHERE login=%s and motPasse=%s"
+        param = (login, mdp)
+        cursor.execute(sql, param)
+        res = convert_dictionnary(cursor)
+        close_bd(cursor,cnx)
+
+    except mysql.connector.Error as err:
+        res = "Failed get verifAuth Data: {}".format(err)
+
+    return res
---
+++ Server/controller/Auth.py
@@ -0,0 +1,26 @@
+from flask import Flask, session, render_template, request, redirect, url_for
+from Server.Data import db
+
+def add_account(request):
+    user = request.form['user']
+    password = request.form['password']
+    return msg
+
+def connect_account(request):
+    login = request.form['login']
+    mdp = request.form['password']
+    print(request)
+    res = db.getAuthData(login,mdp)
+    #print(res)
+    try:
+        session["idUser"] = res[0]['idUser']
+        session["nom"] = res[0]['nom']
+        session["prenom"] = res[0]['prenom']
+        session["login"] = res[0]['login']
+        session["statut"] = res[0]['statut']
+        session["mail"] = res[0]['mail']
+        session["new_mdp"] = res[0]['new_mdp']
+        msg = "succes"
+    except:
+        msg = "echec"
+    return msg
