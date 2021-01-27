e44d6f4b39bcc78ac9d06456308bf2df4e0b1598
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-01-27 17:21:53 +0100

    update db

--- static/js/statistiques.js
+++ static/js/statistiques.js
@@ -18,87 +18,89 @@ function TimeUpdate(DateArr,DateDep,cbk){
     },'json');
 }
 
-    $("input[name='trip-start']").change(function(){
+$("input[name='trip-start']").change(function(){
     // récup 2 dates :
-        let DateArr = $("#start").val();
-        let DateDep = $("#end").val();
-        console.log(DateArr);
-        TimeUpdate(DateArr,DateDep, displayStats);
-//TODO : mettre à jour le graphe
+    let DateArr = $("#start").val();
+    let DateDep = $("#end").val();
+    console.log(DateArr);
+    TimeUpdate(DateArr,DateDep, updateStats);
 
-    });    //installer timeupdate sur les 2 widgets
+});    //installer timeupdate sur les 2 widgets
 
-
-function displayStats(donnees)
-    {
-        let labelsC = donnees.map(function (e)
-        {
-            return "Parking" + e.x
-        });
-
-        let dataC = donnees.map(function (e)
-        {
-            return (e.y)
-        });
-
-
-
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'polarArea',
-            data: {
-                labels: labelsC, //dataC.label (x)
-                datasets: [{
-                    label: "Taux d'occupation des parkings",
-                    data: dataC, //dataC.data (y)
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(155,29,32,0.2)',
-                        'rgba(114,220,96,0.2)',
-                        'rgba(99,93,92,0.2)',
-                        'rgba(208,255,206,0.2)',
-                        'rgba(252,43,255,0.2)',
-                        'rgba(0,150,0,0.2)',
-                        'rgba(0,0,0,0.2)',
-                        'rgba(114,0,96,0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(155,29,32,1)',
-                        'rgba(114,220,96,1)',
-                        'rgba(99,93,92,1)',
-                        'rgba(208,255,206,1)',
-                        'rgba(252,43,255,1)',
-                        'rgba(0,150,0,1)',
-                        'rgba(0,0,0,1)',
-                        'rgba(114,0,96,1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: [], //dataC.label (x)
+        datasets: [{
+            label: "Taux d'occupation des parkings",
+            data: [], //dataC.data (y)
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(155,29,32,0.2)',
+                'rgba(114,220,96,0.2)',
+                'rgba(99,93,92,0.2)',
+                'rgba(208,255,206,0.2)',
+                'rgba(252,43,255,0.2)',
+                'rgba(0,150,0,0.2)',
+                'rgba(0,0,0,0.2)',
+                'rgba(114,0,96,0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(155,29,32,1)',
+                'rgba(114,220,96,1)',
+                'rgba(99,93,92,1)',
+                'rgba(208,255,206,1)',
+                'rgba(252,43,255,1)',
+                'rgba(0,150,0,1)',
+                'rgba(0,0,0,1)',
+                'rgba(114,0,96,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
                 }
-            }
-        });
+            }]
+        }
+    }
+});
+
+function updateStats(donnees)
+{
+    let labelsC = donnees.map(function (e)
+    {
+        return "Parking" + e.x
+    });
+
+    let dataC = donnees.map(function (e)
+    {
+        return (e.y)
+    });
+    console.log(labelsC);
+    console.log(dataC);
+    myChart.data.labels = labelsC;
+    myChart.data.datasets = dataC;
+    myChart.update();
 }
-        let DateArr = $("#start").val();
-        let DateDep = $("#end").val();
-TimeUpdate(DateArr, DateDep, displayStats);
+
+let DateArr = $("#start").val();
+let DateDep = $("#end").val();
+TimeUpdate(DateArr, DateDep, updateStats);
+
 });
